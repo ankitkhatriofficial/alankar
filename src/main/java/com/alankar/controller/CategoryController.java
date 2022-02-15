789384eab42fa4b192a505951c9f72b4e403f193
Ankit Khatri <ankitkhatri@Ankits-MacBook-Pro-2.local>
2022-02-15 15:31:42 +0530

    Added CrossOrigin to Controller

--- src/main/java/com/alankar/controller/CategoryController.java
+++ src/main/java/com/alankar/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.alankar.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -20,6 +21,7 @@ import com.alankar.model.service.CategoryService;
  * @author ankitkhatri
  */
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = RestMappingConst.Category.BASE)
 public class CategoryController {
--- src/main/java/com/alankar/controller/ProductController.java
+++ src/main/java/com/alankar/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.alankar.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,6 +22,7 @@ import com.alankar.model.service.ProductService;
  * @author ankitkhatri
  */
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = RestMappingConst.Product.BASE)
 public class ProductController {
